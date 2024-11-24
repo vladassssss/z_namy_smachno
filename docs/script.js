040e763411811e793d6523b4a8f541be59cbd64c
vladassssss <vladasnizhynska@gmail.com>
2024-11-24 15:39:00 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,6 @@
-     // JavaScript код для зміни кольору заголовка при натисканні кнопки
-        document.getElementById("title").addEventListener("click", function() {
-            const title = document.getElementById("title");
-            title.style.color = "white";  // Зміна кольору заголовка
-            title.style.fontSize = "36px";  // Зміна розміру шрифта
-            title.style.transition = "all 0.5s ease";  // Плавний перехід
-             });
-
+ document.getElementById("title").addEventListener("click", function() {
+    const title = document.getElementById("title");
+    title.style.color = "blue";  // Зміна кольору заголовка
+    title.style.fontSize = "36px";  // Зміна розміру шрифта
+    title.style.transition = "all 0.5s ease";  // Плавний перехід
+});
