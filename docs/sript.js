cb1e2e6be93c385435dd75569cf471e71d6222e7
vladassssss <vladasnizhynska@gmail.com>
2024-11-24 15:29:19 +0200

    Create sript.js

---
+++ docs/sript.js
@@ -0,0 +1,6 @@
+     // JavaScript код для зміни кольору заголовка при натисканні кнопки
+        document.getElementById("title").addEventListener("click", function() {
+            const title = document.getElementById("title");
+            title.style.color = "white";  // Зміна кольору заголовка
+            title.style.fontSize = "36px";  // Зміна розміру шрифта
+            title.style.transition = "all 0.5s ease";  // Плавний перехід
