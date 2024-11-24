1cba214e6052e45fffa9e69263b7c48698109e8c
vladassssss <vladasnizhynska@gmail.com>
2024-11-24 15:44:00 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
  document.getElementById("title").addEventListener("click", function() {
     const title = document.getElementById("title");
-    title.style.color = "blue";  // Зміна кольору заголовка
+    title.style.color = "black";  // Зміна кольору заголовка
     title.style.fontSize = "36px";  // Зміна розміру шрифта
     title.style.transition = "all 0.5s ease";  // Плавний перехід
 });
