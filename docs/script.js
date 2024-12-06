75c24fa949a697f6c58554ce9351a0794ca9208b
vladassssss <vladasnizhynska@gmail.com>
2024-12-06 17:44:49 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,11 @@
-// Зміна стилю заголовка при кліку
+
 document.getElementById('title').addEventListener('click', function () {
     this.style.color = '#ff4500';
     this.style.textShadow = '2px 2px 4px #000';
     this.style.transition = 'all 0.5s ease-in-out';
 });
 
-// Анімація при наведенні на пункт меню
+
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('mouseover', function () {
         this.style.backgroundColor = '#ffcc00';
@@ -18,11 +18,11 @@ document.querySelectorAll('.nav-link').forEach(link => {
     });
 });
 
-// Анімація тексту в розділі
+
 const sectionText = document.querySelector('.section-text');
 sectionText.addEventListener('mouseenter', function () {
     sectionText.style.fontSize = '22px';
-    sectionText.style.color = '#008000';
+    sectionText.style.color = '#A62B2B';
     sectionText.style.transition = 'all 0.5s';
 });
 sectionText.addEventListener('mouseleave', function () {
